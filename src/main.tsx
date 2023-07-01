f6eef4fcbeef0fe06270aede52e156506bd27352
Pedro Otávio <48136045+pedroota@users.noreply.github.com>
2023-07-01 18:09:56 -0300

    feat: Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1 className="text-red-700">oi</h1>
+    <App />
   </React.StrictMode>
 );
