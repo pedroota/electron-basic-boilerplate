1841792040e046c257ee362764ec6d466ba05c00
Pedro Otavio <pedrocoutosoares214@gmail.com>
2023-07-01 18:06:47 -0300

    feat: initial

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <h1 className="text-red-700">oi</h1>
+  </React.StrictMode>
+);
